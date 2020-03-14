8d93cd9831229548b4846090a80530d1dd18ea9c
Michael Hausenblas <hausenbl@amazon.com>
2020-03-14 13:27:58 +0000

    fixes frontend UI
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

--- frontend/noteless.js
+++ frontend/noteless.js
@@ -5,6 +5,7 @@
     let video = document.querySelector('.camera-feed')
     let canvas = document.querySelector('.camera-canvas')
     const result = document.querySelector('#result')
+    const processbtn = document.querySelector('#process')
 
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then((stream) => {
@@ -19,34 +20,28 @@
         height = video.videoHeight / (video.videoWidth / width)
         video.setAttribute('width', width)
         video.setAttribute('height', height)
-
-        setInterval(() => {
-            grabPhoto()
-        }, 5000)
     })
 
-    function grabPhoto() {
+    processbtn.addEventListener('click', () => {
         let context = canvas.getContext('2d')
         canvas.width = width
         canvas.height = height
         context.drawImage(video, 0, 0, width, height)
-
         let data = canvas.toDataURL('image/png')
         // remove the first chunk of "data:image/png;base64,"
         data = data.substring(22)
-
-        fetch('HTTP_API/intake', {
+        // call out to Noteless HTTP API (Lambda functions)
+        fetch('https://st8v3ad9y8.execute-api.eu-west-1.amazonaws.com/v1/intake', {
             method: 'post',
             body: JSON.stringify({
                 Image: data
             })
         })
-            .then((res) => {
-                res.json().then((content) => {
-                    // result = content.TextDetections[0].DetectedText;
-                    console.log(content.TextDetections);
-                    
-                })
+        .then((res) => {
+            res.json().then((content) => {
+                result.innerHTML=content;
+                console.log(content);
             })
-    }
+        })
+    })
 })()
