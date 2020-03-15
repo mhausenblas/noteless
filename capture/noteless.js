18308fe4cd9386cf3a1ec4448b3aaaad1dc81fd5
Michael Hausenblas <hausenbl@amazon.com>
2020-03-15 10:24:33 +0000

    makes capture client device sensitive
    uses back or front camera depending on device type
    
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

--- capture/noteless.js
+++ capture/noteless.js
@@ -7,14 +7,46 @@
     const result = document.querySelector('#result')
     const processbtn = document.querySelector('#process')
 
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
-        .then((stream) => {
-            video.srcObject = stream
-            video.play()
-        })
-        .catch((err) => {
-            console.error(err)
-        })
+    var hasTouchScreen = false;
+    if ("maxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.maxTouchPoints > 0;
+    } else if ("msMaxTouchPoints" in navigator) {
+        hasTouchScreen = navigator.msMaxTouchPoints > 0;
+    } else {
+        var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+        if (mQ && mQ.media === "(pointer:coarse)") {
+            hasTouchScreen = !!mQ.matches;
+        } else if ('orientation' in window) {
+            hasTouchScreen = true; // deprecated, but good fallback
+        } else {
+            // Only as a last resort, fall back to user agent sniffing
+            var UA = navigator.userAgent;
+            hasTouchScreen = (
+                /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+                /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+            );
+        }
+    }
+    if (hasTouchScreen) { // for mobile devices we're using the rear camera
+        navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
+            .then((stream) => {
+                video.srcObject = stream
+                video.play()
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    } else { // for desktop browsers, the default front-facing camera
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then((stream) => {
+                video.srcObject = stream
+                video.play()
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+
+    }
 
     video.addEventListener('canplay', () => {
         height = video.videoHeight / (video.videoWidth / width)
