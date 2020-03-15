2acb0b11a5485f82002895d52b80be5421d2f055
Michael Hausenblas <hausenbl@amazon.com>
2020-03-15 15:00:00 +0000

    inits container-based listings backend
    EKS on Fargate cluster
    
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

---
+++ listings/create-eks-fargate-cluster.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+if ! command -v jq >/dev/null 2>&1; then
+    echo "Please install jq before continuing"
+    exit 1
+fi
+
+if ! command -v eksctl >/dev/null 2>&1; then
+    echo "Please install eksctl before continuing"
+    exit 1
+fi
+
+TARGET_REGION=${1:-eu-west-1}
+CLUSTER_NAME=noteless
+
+echo "Provisioning EKS on Fargate cluster in $TARGET_REGION"
+
+# create EKS on Fargate cluster:
+tmpdir=$(mktemp -d)
+cat <<EOF >> ${tmpdir}/fg-cluster-spec.yaml
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+
+metadata:
+  name: $CLUSTER_NAME
+  region: $TARGET_REGION
+
+iam:
+  withOIDC: true
+
+fargateProfiles:
+  - name: defaultfp
+    selectors:
+      - namespace: serverless
+
+cloudWatch:
+  clusterLogging:
+    enableTypes: ["*"]
+EOF
+eksctl create cluster -f ${tmpdir}/fg-cluster-spec.yaml
+
+# check if cluster if available
+cluster_status="UNKNOWN"
+until [ "$cluster_status" == "ACTIVE" ]
+do 
+    cluster_status=$(eksctl get cluster $CLUSTER_NAME --region $TARGET_REGION -o json | jq -r '.[0].Status')
+    sleep 3
+done
+
+# create serverless namespace for Fargate pods, make it the active namespace:
+echo "EKS on Fargate cluster $CLUSTER_NAME is ready, configuring it:"
+kubectl create namespace serverless
+kubectl config set-context $(kubectl config current-context) --namespace=serverless
---
+++ listings/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"github.com/open-policy-agent/opa/rego"
+)
+
+var (
+
+	// module is the set of Rego rules to detect commands
+	module = `package noteless
+
+		# we declare everything a command that is
+		# 1. at least two characters long, 2. recognized with at least 96% confidence, and
+		# 3. in our command list
+		detected_commands[msg] {
+			dt := input[0].Detections.TextDetections[_].DetectedText
+			confidence := input[0].Detections.TextDetections[_].Confidence
+			count(dt) > 1
+			confidence > 96.0
+			iscommand(dt)
+			msg := sprintf("%v", [lower(dt)])
+		}
+
+		# checks if a word is a command
+		iscommand(candidate) {
+			allcommands := ["go", "stop", "on", "off", "left", "right", "up", "down", "to"]
+			allcommands[_] = lower(candidate)
+		}
+	`
+)
+
+func main() {
+	srv := &http.Server{Addr: ":9898"}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
+			dt, err := detectedTexts()
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+			dc, err := commands(dt)
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+
+			}
+			output, err := json.Marshal(dc)
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write(output)
+		})
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Print("Server Started")
+	<-done
+	log.Print("Server Stopped")
+}
+
+// detectedTexts queries the DDB table for detected texts
+func detectedTexts() ([]interface{}, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})
+	if err != nil {
+		return nil, err
+	}
+	svc := dynamodb.New(sess)
+	result, err := svc.Scan(&dynamodb.ScanInput{
+		TableName: aws.String("Noteless"),
+	})
+	var notes []interface{}
+	for i := range result.Items {
+		var r interface{}
+		err = dynamodbattribute.UnmarshalMap(result.Items[i], &r)
+		if err != nil {
+			return nil, err
+		}
+		notes = append(notes, r)
+	}
+	return notes, nil
+}
+
+// commands returns a list of detected commands
+func commands(input []interface{}) (rego.Result, error) {
+	reg := rego.New(
+		rego.Query("data.noteless.detected_commands"),
+		rego.Module("commands.rego", module),
+		rego.Input(input),
+	)
+	ctx := context.Background()
+	rs, err := reg.Eval(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return rs, nil
+}
