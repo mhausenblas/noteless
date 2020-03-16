9a61dd560c5397ba5fc01ea36b42eb6006674f09
Michael Hausenblas <hausenbl@amazon.com>
2020-03-16 14:11:39 +0000

    fixes ALB setup script doc
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

--- listings/create-alb.sh
+++ listings/create-alb.sh
@@ -3,7 +3,7 @@
 ################################################################################
 ### This script is based on information found in:
 ### - https://kubernetes-sigs.github.io/aws-alb-ingress-controller/guide/controller/setup/
-### - https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
+### - https://aws.amazon.com/blogs/containers/using-alb-ingress-controller-with-amazon-eks-on-fargate/
 ###
 
 set -o errexit
